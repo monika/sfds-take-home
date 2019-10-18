c0026eab6ee1eb034af693e1571c6cf666d88a4b
Monika <monikahoex@gmail.com>
2019-10-17 22:25:09 -0700

    Move content blocks into their own includes

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy('src/images');
 
   return {
-    dir: { input: 'src', output: 'dist', data: '_data' },
+    dir: { input: 'src', output: 'dist', data: '_data', includes: '_includes' },
     passthroughFileCopy: true,
     templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
     htmlTemplateEngine: 'njk'
-  }
-}
+  };
+};
